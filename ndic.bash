d5d3f46dfb5c9aa888fb7d740a0f15976d6c0aca
Ohgyun Ahn <ohgyun@gmail.com>
2013-01-16 18:34:06 +0900

    replace space to %20

--- ndic.bash
+++ ndic.bash
@@ -3,7 +3,7 @@
 # ndic <word>
 # Search the word from dictionary
 
-if [[ -z "$BASH" ]]; then
+if [[ -z $BASH ]]; then
   cat >&2 <<MSG
 Ndic is a bash program, and musb be run with bash.
 MSG
@@ -11,28 +11,32 @@ MSG
 fi
 
 main () {
-  if [[ -z "$1" ]]; then
+  if [[ -z $1 ]]; then
     echo "usage: ndic <word>"
   fi
   search "$1"
 }
 
 search () {
-  local url=`get_url $1`
-  local result=`curl -s $url`
+  local url
+  local result
+
+  url=`get_url "$1"`
+  result=`curl -s "$url"`
+  
   print_result "$result"
 }
 
 get_url () {
   # use mobile assistant dictionary
-  echo "http://m.endic.naver.com/assistDic.nhn?query=$1"
+  echo "http://m.endic.naver.com/assistDic.nhn?query=${1// /%20}"
 }
 
 print_result () {
   local str="$1"
   local regex='<p class="ly_p">([^<]+)</p>'
 
-  while [[ "$str" =~ $regex ]]; do
+  while [[ $str =~ $regex ]]; do
     # print result
     echo "${BASH_REMATCH[1]}"
 
