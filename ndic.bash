c0fe22292f2e9bfe07cb6bb471408a68f9eb5b99
Ohgyun Ahn <ohgyun@gmail.com>
2013-01-17 00:33:03 +0900

    add options for debugging

--- ndic.bash
+++ ndic.bash
@@ -10,31 +10,65 @@ MSG
   exit 1
 fi
 
+word=''
+is_debug=false
+
 main () {
   if [[ -z $1 ]]; then
-    echo "usage: ndic <word>"
+    print_help 
+    exit 1
   fi
-  search "$1"
+
+  # reset option index
+  OPTIND=1
+
+  # parse arguments
+  while getopts 'hd' opt; do
+    case $opt in
+      h)
+        print_help
+        exit 0
+        ;;
+      d) is_debug=true
+        ;;
+      *) echo 'invalid options error...';;
+    esac
+  done
+
+  word=${@:$OPTIND}
+
+  debug "WORD: $word"
+
+  search
+}
+
+debug () {
+  if $is_debug; then
+    echo "$@"
+  fi
 }
 
 search () {
-  local url
-  local result
+  local url result
 
-  url=`get_url "$1"`
+  url=`get_url`
   result=`curl -s "$url"`
   
+  debug "URL: $url"
+
   print_result "$result"
 }
 
 get_url () {
   # use mobile assistant dictionary
-  echo "http://m.endic.naver.com/assistDic.nhn?query=${1// /%20}"
+  echo "http://m.endic.naver.com/assistDic.nhn?query=${word// /%20}"
 }
 
 print_result () {
   local str="$1"
   local regex='<p class="ly_p">([^<]+)</p>'
+
+  debug "HTML: $str"
 
   while [[ $str =~ $regex ]]; do
     # print result
@@ -43,6 +77,22 @@ print_result () {
     # delete matched string
     str=${str/"${BASH_REMATCH}"/}
   done
+}
+
+print_help () {
+  cat <<MSG
+
+Usage: ndic [-hd] <word>
+
+Description:
+  Find the meaning of <word> in English-Korean Dictionary.
+  (Powered by Naver)
+
+Options:
+  -h  Show help message
+  -d  Turn on debug mode
+
+MSG
 }
 
 main "$@"
