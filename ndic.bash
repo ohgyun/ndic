21a979351443ae5d199dd17135b19e2adbee176a
Ohgyun Ahn <ohgyun@gmail.com>
2013-01-17 00:37:15 +0900

    modify help message

--- ndic.bash
+++ ndic.bash
@@ -92,6 +92,14 @@ Options:
   -h  Show help message
   -d  Turn on debug mode
 
+Examples:
+  $ ndic nice
+  [형용사](기분) 좋은, 즐거운, 멋진
+  [명사]니스 ((프랑스 남동부의 피한지))
+
+  $ ndic "good thing" # use quotes if a word has spaces
+  [구어] 좋은 일; 좋은 착상; 행운; 경구; 진미; 사치품
+
 MSG
 }
 
